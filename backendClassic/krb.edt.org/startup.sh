93d352a7bdf879498a65baa47f860ac49d61a371
isx434324 <tg.alva1@gmail.com>
2018-05-14 14:01:11 +0200

    servei ftp kerberitzat

--- backendClassic/krb.edt.org/startup.sh
+++ backendClassic/krb.edt.org/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/sbin/krb5kdc -P /var/run/krb5kdc.pid $KRB5KDC_ARGS && echo "kdc started"
-/usr/sbin/kadmind -nofork -P /var/run/kadmind.pid $KADMIND_ARGS && echo "kadmin started"
+/usr/sbin/krb5kdc -P /var/run/krb5kdc.pid $KRB5KDC_ARGS
+/usr/sbin/kadmind -nofork -P /var/run/kadmind.pid $KADMIND_ARGS
