2b8cbcdd0f3339ef2219032d06457a8ffd7429d4
root <root@localhost.localdomain>
2018-05-20 03:03:06 +0200

    krbldap.edt.org update

--- backendLDAP/krbldap.edt.org/startup.sh
+++
@@ -1,4 +0,0 @@
-export TERM=xterm-color
-/usr/sbin/slapd -u ldap -h "ldap:/// ldaps:/// ldapi:///"
-/usr/sbin/krb5kdc -P /var/run/krb5kdc.pid $KRB5KDC_ARGS
-/usr/sbin/kadmind -nofork -x binddn=cn=admin,dc=edt,dc=org -P /var/run/kadmind.pid $KADMIND_ARGS
--- backendLDAP/krbldap.edt.org/startupldap.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-export TERM=xterm-color
-/usr/sbin/slapd -d0 -u ldap -h "ldap:/// ldaps:/// ldapi:///"
